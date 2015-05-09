f24579383f6e5cdc41f1a5e11091d84ab7bd9511
bkazic <blaz.kazic@ijs.si>
2015-05-09 12:33:54 +0200

    Test with express

--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -1,6 +1,18 @@
-﻿var http = require('http');
+﻿/*
+var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port);
+}).listen(port)
+*/
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send("Hello world from server.js");
+})
+
+app.listen(3000);
+console.log("Server running on port 3000");
